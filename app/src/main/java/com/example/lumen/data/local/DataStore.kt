28dcbfcddd03c01f4ec0ef8b876e05e3176c4af7
Ramiz <63073421+ramizwd@users.noreply.github.com>
2025-10-17 18:47:45 +0300

    handling DataStore file corruption just in case :)

--- app/src/main/java/com/example/lumen/data/local/DataStore.kt
+++ app/src/main/java/com/example/lumen/data/local/DataStore.kt
@@ -2,7 +2,16 @@ package com.example.lumen.data.local
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
+import timber.log.Timber
 
-val Context.colorDataStore: DataStore<Preferences> by preferencesDataStore("color_prefs")
+val Context.colorDataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "color_prefs",
+    corruptionHandler = ReplaceFileCorruptionHandler { exception ->
+        Timber.tag("DataStore").e(exception, "DataStore corrupted")
+        emptyPreferences()
+    }
+)
