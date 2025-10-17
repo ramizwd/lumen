88f8d81500e350126186960f441d521a2ea8fffe
Ramiz <63073421+ramizwd@users.noreply.github.com>
2025-10-17 03:10:18 +0300

    handle error

--- app/src/main/java/com/example/lumen/domain/ble/usecase/control/SaveCustomColorUseCase.kt
+++ app/src/main/java/com/example/lumen/domain/ble/usecase/control/SaveCustomColorUseCase.kt
@@ -2,12 +2,21 @@ package com.example.lumen.domain.ble.usecase.control
 
 import com.example.lumen.domain.ble.ColorPreferenceManager
 import com.example.lumen.domain.ble.model.CustomColorSlot
+import timber.log.Timber
 import javax.inject.Inject
 
 class SaveCustomColorUseCase @Inject constructor(
     private val colorPreferenceManager: ColorPreferenceManager
 ) {
+    companion object {
+        private const val LOG_TAG = "SaveCustomColorUseCase"
+    }
+
     suspend operator fun invoke(deviceAddress: String, slot: CustomColorSlot) {
-        colorPreferenceManager.saveCustomColor(deviceAddress, slot)
+        try {
+            colorPreferenceManager.saveCustomColor(deviceAddress, slot)
+        } catch (e: IllegalArgumentException) {
+            Timber.tag(LOG_TAG).e(e, "Error saving color")
+        }
     }
 }
