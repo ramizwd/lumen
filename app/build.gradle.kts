838fde036492ffe6219a693056ff91e21c8dc6f1
ramizwd <63073421+ramizwd@users.noreply.github.com>
2026-08-19 23:39:00 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,7 @@ dependencies {
     testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
 
     // Debugging & Preview
