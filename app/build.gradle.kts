ccccfba45a450dc3c9301a96ae1ccc9118f18785
ramizwd <63073421+ramizwd@users.noreply.github.com>
2026-08-21 17:10:28 +0300

    build: version bump to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.example.lumen"
         minSdk = 31
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
